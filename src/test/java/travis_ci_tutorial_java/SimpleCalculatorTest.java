9a88d776275011207109bb65dce9baeee148c994
matitesfu27 <146690983+matitesfu27@users.noreply.github.com>
2023-12-13 17:16:47 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,6 +9,6 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(0,calc.add(1, 1), 2);
+		assertEquals(0,calc.add(2,calculator.minus(5,3));
 	}
 }
